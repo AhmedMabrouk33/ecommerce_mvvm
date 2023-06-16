6ec854f30fbb997333a1bbf2dd32ce3eb9642df0
ahmed mabrouk <ahmedmabrouk3394@gmail.com>
2023-06-16 17:03:55 +0300

    App bar widget

--- app/lib/utils/widgets/app_bar/sticky_app_bar.dart
+++ app/lib/utils/widgets/app_bar/sticky_app_bar.dart
@@ -1,1 +1,63 @@
+import 'package:flutter/material.dart';
 
+class StickyAppBar {
+  static Widget buildAppBar({
+    required String imageNetworkUrl,
+    required VoidCallback popAction,
+    required VoidCallback favouriteAction,
+    required bool isFavourite,
+  }) {
+    return Container(
+      height: 189,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        image: imageNetworkUrl.isNotEmpty
+            ? DecorationImage(
+                image: NetworkImage(imageNetworkUrl),
+                fit: BoxFit.fitHeight,
+              )
+            : null,
+      ),
+      child: Container(
+        height: 60,
+        width: double.infinity,
+        color: const Color.fromRGBO(217, 217, 217, 0.1),
+        padding: const EdgeInsets.only(left: 15, right: 15, top: 60),
+        alignment: Alignment.topCenter,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            InkWell(
+              onTap: popAction,
+              child: const CircleAvatar(
+                radius: 22.5,
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.black,
+                child: Icon(
+                  Icons.chevron_left_outlined,
+                  color: Colors.black,
+                  size: 40,
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: favouriteAction,
+              child: const CircleAvatar(
+                radius: 22.5,
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.black,
+                child: Icon(
+                  Icons.star_border_purple500_outlined,
+                  size: 40,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
