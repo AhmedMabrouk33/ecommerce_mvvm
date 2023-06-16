ad7280ce816f92056bff8e2c2f9e4ac5b2b7b4e0
ahmed mabrouk <ahmedmabrouk3394@gmail.com>
2023-06-16 08:37:49 +0300

    Add Product Repository in application configuration repossitory

--- app/lib/core/data/repository_configuration.dart
+++ app/lib/core/data/repository_configuration.dart
@@ -1,5 +1,3 @@
-
-
 import '../../features/account/location/repositories/location_test.dart';
 import '../../features/account/location/repositories/location_repository.dart';
 
@@ -9,8 +7,12 @@ import '../../features/account/profile/repositories/profile_repository.dart';
 import '../../features/explore/repositories/category/category_test.dart';
 import '../../features/explore/repositories/category/category_repository.dart';
 
+import '../../features/explore/repositories/product/product_test.dart';
+import '../../features/explore/repositories/product/product_repository.dart';
+
 class ApplicationRepositories {
   static const ProfileRepository profileRepository = ProfileTest();
   static const LocationRepository locationRepository = LocationTest();
   static const CategoryRepository categoryRepository = CategoryTest();
+  static const ProductRepository productRepository = ProductTest();
 }
